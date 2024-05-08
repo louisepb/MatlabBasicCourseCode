b5a35233ab6e658d29f9e91b630a8c99d42629de
Louise Brown <louise.brown@nottingham.ac.uk>
2024-05-08 14:26:55 +0100

    Converted Freefall and PlotPolynomial to live scripts

--- Examples/DistBetweenPoints.m
+++ Examples/DistBetweenPoints.m
@@ -1,12 +1,6 @@
-function distance = DistBetweenPoints( x1, y1, x2, y2 )
-% DistBetweenPoints - Function to calculate the distance between
-% two cartesian points (x1,y1) and (x2,y2)
-%
-% Function returns the distance between the points
-% Format:
-% distance = DistBetweenPoints( x1, y1, x2, y2 )
-
-%Calculate distance
-distance = sqrt( (x2 - x1)^2 + (y2 - y1)^2 );
-
+function [distance, x1] = DistBetweenPoints(x1,y1,x2,y2)
+%Function to calculate line length
+%   Detailed explanation goes here
+    distance = sqrt( (x2-x1)^2 + (y2 - y1)^2 );
+    x1 = x1+10;
 end
--- Examples/FreefallExample.m
+++ Examples/FreefallExample.m
@@ -1,16 +1,16 @@
-%Freefall example script
- 
-%Define varibles g, time, distance
+%Script to calculate position of body in freefall
+
+% User input for end time
+endTime = input('Please input end time: ');
+
+%Create a vector of times between start and end time
+Times = linspace(0, endTime, 10);
+
+% Initialise g
 g = 9.81;
 
-% User input of final time
-lastTime = input('Please input final time ');
+% Calculate distances
+distances = 0.5*g*(Times.*Times);
 
-% Time increments vector
-times = linspace(0, lastTime, 10);
-
-%Calculate distances
-distances = 0.5*g*times.*times;
-
-% Output distances
+%Display distances
 disp(distances);
